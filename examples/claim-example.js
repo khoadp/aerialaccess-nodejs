3c3510978d4de71daa7c3bc5b78d3d3397f58b31
khoadp <dkhoa81@gmail.com>
2020-10-26 18:51:48 -0700

    APIs Skeleton

--- examples/claim-example.js
+++ examples/claim-example.js
@@ -1,10 +1,15 @@
 const aerialaccess = require('aerialaccess-nodejs')
 
 // Initialize claim service
-const apiKey = 'API_KEY'
 const baseUrl = 'BASE_URL' // https://fhir.medicision.com
-const claimService = new aerialaccess.Claim(baseUrl, apiKey);
+const claimService = new aerialaccess.Claim(baseUrl)
 
+// Get access token
+const username = "USER_NAME";
+const password = "PASSWORD"
+const token = claimService.getJWTToken(username, password);
+
+// Execute claim service
 const identifier = 123
-const claimResponse = claimService.getClaim(identifier)
+const claimResponse = claimService.getClaim(token, identifier)
 console.log(claimResponse)
--- examples/patient-example.js
+++ examples/patient-example.js
@@ -1,11 +1,15 @@
 const aerialaccess = require('aerialaccess-nodejs')
 
 // Initialize patient service
-const apiKey = 'API_KEY'
 const baseUrl = 'BASE_URL' // https://fhir.medicision.com
-const patientService = new aerialaccess.Patient(baseUrl, apiKey);
+const patientService = new aerialaccess.Patient(baseUrl);
+
+// Get access token
+const username = "USER_NAME";
+const password = "PASSWORD"
+const token = patientService.getJWTToken(username, password);
 
 // Execute patient service
 const identifier = 123
-const patientResponse = patientService.getPatient(identifier)
+const patientResponse = patientService.getPatient(token, identifier)
 console.log(patientResponse)
